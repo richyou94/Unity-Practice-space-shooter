4c15f26da1eda1976c39e0f74b4ad8047c1eb586
Richard You <richardyou94@gmail.com>
2023-03-06 13:36:46 -0800

    able to move up down and left right

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    //public or private reference
+    //data type(int, float, bool, string)
+    // every variable has a name
+    //optional value assigned
+    [SerializeField]
+    private float speed = 3.5f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +21,12 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+
+        // new Vector3(1, 0, 0);
+        transform.Translate(Vector3.right * horizontalInput * speed * Time.deltaTime);
+        transform.Translate(Vector3.up * verticalInput * speed * Time.deltaTime);
     }
 }
